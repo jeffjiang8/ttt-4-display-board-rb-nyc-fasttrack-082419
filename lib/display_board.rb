1f9040dd02e0313e307a85a6a785a94d93dddd31
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-08-24 17:50:44 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,15 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------" 
+  puts "   |   |   "
+end
+
+board_one = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+display_board(board_one)
+
+board_two = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+display_board(board_two)
