cb567df1104033863e7513adf6ef6aa72f130169
Jinfeng Jiang <jeffjiang8@gmail.com>
2019-08-24 18:03:10 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,5 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = [" "," "," "," ","X"," "," "," "," "]
-
 def display_board(board)
   puts "#{board[0]}|#{board[1]}|#{board[2]}"
   puts "-----------"
@@ -10,4 +8,5 @@ def display_board(board)
   puts "#{board[6]}|#{board[7]}|#{board[8]}"
 end
 
+board = [" "," "," "," "," "," "," "," "," "]
 display_board(board)
